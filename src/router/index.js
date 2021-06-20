5eca03b5c52607508a23b5f48f646317799faf34
Yuying <syuying07@163.com>
2021-06-20 16:41:27 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+// 安装插件
+Vue.use(VueRouter)
+
+// 创建router
+const routes=[
+    
+]
+const router=new VueRouter({
+    routes,
+    mode:'history'
+})
+
+export default router
