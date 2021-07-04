6e0016b24362ceb3148816c60a3272f4bd795d3c
Yuying <syuying07@163.com>
2021-07-04 11:03:21 +0800

    update

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>主页</div>
+    <div id="home">
+        <nav-bar><div slot="center">购物街</div></nav-bar>
+    </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-
+    name:"Home",
+    components:{
+        NavBar
+    }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
