6096ff8312e726f14a558578b863c83bbdf3b4d2
Yuying <syuying07@163.com>
2021-07-11 21:24:20 +0800

    update

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -37,15 +37,17 @@ export default {
         })
         this.scroll.on('pullingUp',()=>{
             this.$emit('pullingUp')
-            
         })
     },
     methods:{
         scrollTo(x,y,time=300){
-            this.scroll.scrollTo(x,y,time)
+            this.scroll && this.scroll.scrollTo(x,y,time) //逻辑语
         },
         finishPullUp(){
-            this.scroll.finishPullUp()
+            this.scroll && this.scroll.finishPullUp()
+        },
+        refresh(){
+            this.scroll && this.scroll.refresh()
         }
     }
     
