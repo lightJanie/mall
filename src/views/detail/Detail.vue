971d8e52e6cf821d0e347baa3aedd692f09f7505
Yuying <syuying07@163.com>
2021-07-19 22:54:29 +0800

    update

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="detail">
-     <detail-nav-bar></detail-nav-bar>
-     <detail-swiper :top-images="topImages" />
+  <div class="detail">
+     <detail-nav-bar />
+     <scroll class="content" ref="scroll">
+         <detail-swiper :top-images="topImages" />
+         <detail-base-info :goods="goodsInfo"></detail-base-info>
+     </scroll>
+     <h2>详情页:{{iid}}</h2>
   </div>
 </template>
 
 <script>
 import detailNavBar from './childComps/detailNavBar'
 import detailSwiper from './childComps/detailSwiper'
+import detailBaseInfo from './childComps/DetailBaseInfo'
+
+import Scroll from 'components/common/scroll/Scroll'
 
 import {getDetail,Goods} from 'api/detail'
 
@@ -15,13 +22,15 @@ export default {
     name:'Detail',
     components:{
         detailNavBar,
-        detailSwiper
+        detailSwiper,
+        detailBaseInfo,
+        Scroll
     },
     data(){
         return {
             iid:null,
             topImages:[],
-            goods:null,
+            goodsInfo:{},
         }
     },
     created(){
@@ -29,14 +38,26 @@ export default {
         getDetail(this.iid).then(res=>{
             const data=res.result
             this.topImages=data.itemInfo.topImages
-            this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+            this.goodsInfo = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
             
             
         })
+    },
+    methods:{
+        
     }
 }
 </script>
 
 <style>
-
+.detail{
+    height:100vh;
+    background-color: #fff;
+    position:relative;
+    z-index:1;
+}
+.content{
+    background-color: #fff;
+    height:500px;
+}
 </style>
